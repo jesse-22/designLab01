a21f57690d38dd8e97853ddbe51f0e5a6617e4c8
jessechowning <chowningjesse@gmail.com>
2023-12-10 21:50:11 -0500

    cleaned up code

--- designLab01Work.py
+++ designLab01Work.py
@@ -1,10 +1,3 @@
-#
-# File:   designLab01Work.py
-# Author: 6.01 Staff
-# Date:   02-Sep-11
-#
-# Below are templates for your answers to three parts of Design Lab 1
-
 # -----------------------------------------------------------------------------
 
 def fib(n):
